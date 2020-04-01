cac6f0d2b1b91c48e9c6c1659f34d0bc197788e9
SuselMan <i.pavluhin@ya.ru>
2020-04-01 20:01:29 +0300

    Add missed files set

---
+++ src/server/game/config.ts
@@ -0,0 +1,19 @@
+import 'phaser'
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.HEADLESS,
+  parent: 'phaser-game',
+  width: 1280,
+  height: 720,
+  banner: false,
+  // @ts-ignore
+  audio: false
+}
+export default config
+
+export const arcadePhysics = {
+  default: 'arcade',
+  arcade: {
+    gravity: { y: 0 }
+  }
+}
---
+++ src/server/game/game.ts
@@ -0,0 +1,34 @@
+import '@geckos.io/phaser-on-nodejs'
+import commonConfig, { arcadePhysics } from './config'
+
+import ArcadeScene from './scenes/arcadeScene'
+import RoomManager from '../managers/roomManager'
+
+export class PhaserGame extends Phaser.Game {
+  constructor(config: Phaser.Types.Core.GameConfig) {
+    super(config)
+  }
+}
+
+const Game = (roomManager: RoomManager, roomId: string, options: { scene: string; level: number }) => {
+  let config = { ...commonConfig }
+
+  if (options.scene === 'ArcadeScene') {
+    config.scene = [ArcadeScene]
+    config.physics = arcadePhysics
+  }
+
+  // @ts-ignore
+  config.customEnvironment = true
+
+  // a very hackie trick to pass some custom data
+  // but it work well :)
+  config.callbacks = {
+    preBoot: () => {
+      return { level: +options.level, roomManager, roomId }
+    }
+  }
+
+  return new PhaserGame(config)
+}
+export default Game
